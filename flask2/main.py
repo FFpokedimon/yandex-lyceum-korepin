1d0da41b98de31822a9bf2a6b85651369356261d
FFpokedimon <krepkiydima666@gmail.com>
2026-03-31 18:30:18 +0300

    Training in flight

--- flask2/main.py
+++ flask2/main.py
@@ -316,5 +316,10 @@ def carousel():
 '''
 
 
+@app.route('/training/<string:prof>')
+def training(prof):
+    return render_template("training.html", prof=prof)
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080)
