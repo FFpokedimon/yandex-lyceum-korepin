0be6eba9fdef99e6bc752dea5fe4f5ad01a3c762
FFpokedimon <krepkiydima666@gmail.com>
2026-03-31 19:18:56 +0300

    Profession list

--- flask2/main.py
+++ flask2/main.py
@@ -321,5 +321,10 @@ def training(prof):
     return render_template("training.html", prof=prof)
 
 
+@app.route('/list_prof/<string:list_type>')
+def list_prof(list_type):
+    return render_template("list_prof.html", list_type=list_type)
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080)
